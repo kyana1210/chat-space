90db8c955a45e24650079a58304d0b1ddcbd566b
kyana1210 <y.kanaoka121@gmail.com>
2019-08-28 03:56:28 +0900

    edit gruop
    問5作成したグループをサイドバーに表示　
    問6グループ編集機能の実装
    問7ルートパスの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   def show
     @name = current_user.name
   end
-  
+
   def edit
   end
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to root_path 
     else
       render :edit
     end
